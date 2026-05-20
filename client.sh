7171163dd85499853bb5cb4ca9dc65a1d86b8928
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-20 10:21:48 -0500

    fix client.sh for the millionth time

--- client.sh
+++ client.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ssh -T git@github.com
 while true; do
-	git pull
-	cat comms.txt
-done
-while true; do
 	clear
+	git pull
+	clear
+	cat comms.txt
 	read userInput 
 	echo $userInput >> comms.txt
 	git add comms.txt
