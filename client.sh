60b25b3ec87ed3c6a510fec1c6502aec69f71c5e
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-20 10:28:25 -0500

    fix client.sh

--- client.sh
+++ client.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 ssh -T git@github.com
 while true; do
-	clear
 	git pull
 	clear
 	cat comms.txt
-	read userInput 
+done
+while true; do
+	read -t 0.1 userInput 
 	echo $userInput >> comms.txt
 	git add comms.txt
 	git commit -m "message sent"
 	git push
+	clear
+	cat comms.txt
 done
